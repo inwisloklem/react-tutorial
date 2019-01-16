88875a9e0171f4a601099265951b5f2cb5ab7c44
Ilya Filippov <89099109104@mail.ru>
2019-01-16 18:55:04 +0300

    WIP: Lift state from Board to Game component

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,38 +1,16 @@
 import React, { useState, useCallback } from 'react'
 import Square from '../Square'
-import calculateWinner from '../../helpers/calculateWinner'
 import styles from './index.module.sass'
 
-export default function Board () {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-
-  const handleClick = i =>
-    useCallback(() => {
-      if (calculateWinner(squares) || squares[i]) {
-        return
-      }
-
-      const newSquares = [...squares]
-      newSquares[i] = xIsNext ? 'X' : 'O'
-
-      setSquares(newSquares)
-      setXIsNext(!xIsNext)
-    })
-
+export default function Board (props) {
   const renderSquare = i =>
     <Square
-      value={squares[i]}
-      onClick={handleClick(i)}
+      value={props.squares[i]}
+      onClick={props.onClick(i)}
     />
-
-  const winner = calculateWinner(squares)
 
   return (
     <div>
-      <div className={styles.results}>
-        {winner ? 'Winner:' : 'Next player:'} <strong>{winner !== null ? winner : xIsNext ? 'X' : 'O'}</strong>
-      </div>
       <div className={styles.boardRow}>
         {renderSquare(0)}
         {renderSquare(1)}
@@ -50,4 +28,9 @@ export default function Board () {
       </div>
     </div>
   )
+}
+
+Board.propTypes = {
+  squares: PropTypes.array,
+  onClick: PropTypes.any
 }
--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from '../Board'
+import calculateWinner from '../../helpers/calculateWinner'
 import styles from './index.module.sass'
 
-export default function Game () {
+export default function Game() {
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null)
+  }])
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const handleClick = i =>
+    useCallback(() => {
+      const current = history[history.length - 1]
+      const squares = [...current.squares]
+
+      if (calculateWinner(squares) || squares[i]) {
+        return
+      }
+
+      squares[i] = xIsNext ? 'X' : 'O'
+
+      setHistory([...history, squares])
+      setXIsNext(!xIsNext)
+    })
+
+  const current = history[history.length - 1]
+  const winner = calculateWinner(squares)
+
   return (
     <div className={styles.game}>
       <div className={styles.gameBoard}>
-        <Board />
+        <Board
+          squares={current.squares}
+          onClick={i => handleClick(i)}
+        />
       </div>
       <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
+        <div>{winner ? 'Winner:' : 'Next player:'} <strong>{winner !== null ? winner : xIsNext ? 'X' : 'O'}</strong></div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
