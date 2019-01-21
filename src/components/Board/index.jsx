68058d3fd71d20019b84220ae314cfe2d86d803d
inwisloklem <89099109104@mail.ru>
2019-01-21 09:01:47 +0300

    Refactor component

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,31 +1,20 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Square from '../Square'
 import styles from './index.module.sass'
 
 export default function Board (props) {
-  const renderSquare = i =>
-    <Square
-      value={props.squares[i]}
+  const renderSquares = props.squares.map((square, i) => {
+    return <Square
+      value={square}
+      key={i}
       onClick={props.onClick(i)}
     />
+  })
 
   return (
-    <div>
-      <div className={styles.boardRow}>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className={styles.boardRow}>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className={styles.boardRow}>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+    <div className={styles.board}>
+      {renderSquares}
     </div>
   )
 }
